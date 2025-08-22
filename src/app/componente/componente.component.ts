891bc6e18b1e510bc1df2adb074257b0b02e6270
LimasGuy <GustavoLimasGuy@gmail.com>
2025-08-22 03:17:10 -0300

    Lógica parcialmente finalizada e layout definido

--- src/app/componente/componente.component.ts
+++ src/app/componente/componente.component.ts
@@ -1,31 +1,48 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
+
+
 
 @Component({
   selector: 'app-componente',
+  standalone: true,
   imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './componente.component.html',
-  styleUrl: './componente.component.css'
+  styleUrl: './componente.component.css',
+
 })
 export class ComponenteComponent {
 
 form = new FormGroup({
 
-  nome: new FormControl
+  nome: new FormControl('',Validators.required),
+  idade: new FormControl('',Validators.required),
+  especialidade: new FormControl('',Validators.required)
 
 })
 
+erro: string = ''
+generating = false
 active = true
 
+
 agir(){
-
-if (this.active === false){
-  this.active = true
-} else { this.active = false}
-
-
+  if (this.active === false){
+    this.active = true
+  } else { this.active = false}
 }
+
+gerar(){
+  if(this.form.valid){
+  this.generating = true
+  this.erro = ''
+  }else{
+    this.erro = "Preencha os campos corretamente!";
+  }
+}
+
+
 
 }
 
