5b1d7f71e32b4061094b73f050703b65250f14aa
LimasGuy <GustavoLimasGuy@gmail.com>
2025-08-24 10:58:47 -0300

    Final commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,9 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { ComponenteComponent } from "./componente/componente.component";
-import { Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, ComponenteComponent],
+  imports: [ComponenteComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
